43b0eb44cfda31b0d32aa4e1eaf9441a246a0818
kong-hen <100201531+kong-hen@users.noreply.github.com>
2022-12-27 10:32:51 +0800

    Update database.php

--- 空痕取图后端/includes/database.php
+++ 空痕取图后端/includes/database.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = "127.0.0.1";//数据库地址
 $dbuname = "khqt";//数据库用户名
-$dbpword = "k6bA5cfBPGkGP5AG";//数据库密码
+$dbpword = "*********";//数据库密码
 $dbname = "khqt";//数据库名
 $connect = mysqli_connect($dbhost,$dbuname,$dbpword,$dbname);
 ?>
